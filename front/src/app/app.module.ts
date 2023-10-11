e8fa68c509f1dc1c496f2b9a85f0a6c6b738a42a
batsheva <batsheva7424@gmail.com>
2023-10-11 16:01:40 +0300

    basic topography components

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/topography/header/header.component';
+import { FooterComponent } from './components/topography/footer/footer.component';
+import { SidebarComponent } from './components/topography/sidebar/sidebar.component';
+import { LoaderComponent } from './components/topography/loader/loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
